9d517e006911d3e3a02de5dd48c143a63f21de28
jinuuukim <kkjjww1503@naver.com>
2024-11-23 22:15:59 +0900

    차트 화면

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,2 +1,147 @@
-//그냥 차트 사진 아무거나 삽입해놓기
-//진우 차트
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 210),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                decoration: BoxDecoration(
+                  color: Color(0xFFD7F0E7),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TableCalendar(
+                  firstDay: DateTime(2020, 1, 1),
+                  lastDay: DateTime(2030, 12, 31),
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) {
+                    return isSameDay(_selectedDay, day);
+                  },
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  },
+                  calendarStyle: CalendarStyle(
+                    todayDecoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      shape: BoxShape.circle,
+                    ),
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                    ),
+                    defaultTextStyle: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Container(
+                  height: 300,
+                  child: LineChart(
+                    LineChartData(
+                      gridData: FlGridData(show: true),
+                      titlesData: FlTitlesData(
+                        show: true,
+                        topTitles: AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        rightTitles: AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                      ),
+                      borderData: FlBorderData(show: true),
+                      minX: 1,
+                      maxX: 12,
+                      minY: 0,
+                      maxY: 100,
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots: [
+                            FlSpot(1, 50),
+                            FlSpot(2, 60),
+                            FlSpot(3, 60),
+                            FlSpot(4, 50),
+                            FlSpot(5, 55),
+                            FlSpot(6, 60),
+                            FlSpot(7, 45),
+                            FlSpot(8, 75),
+                            FlSpot(9, 50),
+                            FlSpot(10, 55),
+                            FlSpot(11, 45),
+                            FlSpot(12, 50),
+                          ],
+                          isCurved: true,
+                          color: Colors.blue,
+                          dotData: FlDotData(show: false),
+                          belowBarData: BarAreaData(show: false),
+                        ),
+                        LineChartBarData(
+                          spots: [
+                            FlSpot(1, 40),
+                            FlSpot(2, 45),
+                            FlSpot(3, 55),
+                            FlSpot(4, 35),
+                            FlSpot(5, 40),
+                            FlSpot(6, 40),
+                            FlSpot(7, 60),
+                            FlSpot(8, 50),
+                            FlSpot(9, 65),
+                            FlSpot(10, 40),
+                            FlSpot(11, 45),
+                            FlSpot(12, 55),
+                          ],
+                          isCurved: true,
+                          color: Colors.red,
+                          dotData: FlDotData(show: false),
+                          belowBarData: BarAreaData(show: false),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
